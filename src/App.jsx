792e80a83bb1d248ff3aefa1c209664af9df653b
Chandan Kumar <iiitchandan25@gmail.com>
2024-11-08 01:02:46 +0530

    first commit , project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Welcome</h1>
+     <Button>Subscribe</Button>
+      
+    </>
+  )
+}
+
+export default App
